02314ff7c2b2bb13e95b311e02cd88e7abebfbb3
Chris Lange <chris_lange_89@hotmail.com>
2021-12-16 08:26:21 +1000

    inital commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBGj94a1yV1qdll-WApE3tQMHkUoURxZss",
+  authDomain: "react-firebase-photo-app.firebaseapp.com",
+  projectId: "react-firebase-photo-app",
+  storageBucket: "react-firebase-photo-app.appspot.com",
+  messagingSenderId: "201057498810",
+  appId: "1:201057498810:web:3006b382d7b9625adfb483",
+};
+const app = initializeApp(firebaseConfig);
